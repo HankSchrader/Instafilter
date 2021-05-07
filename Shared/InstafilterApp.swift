52154ced33a19539731f635c8f71c473a1113486
Erik Mikac <mikacew@gmail.com>
2021-05-07 07:52:16 -0400

    Initial Commit

---
+++ Shared/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 5/7/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
