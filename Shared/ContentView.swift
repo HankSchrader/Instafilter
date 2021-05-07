37469f7f7a3538d2761550703540f0f01923d88b
Erik Mikac <mikacew@gmail.com>
2021-05-07 08:32:45 -0400

    completed custom binding tutorial: https://www.hackingwithswift.com/books/ios-swiftui/creating-custom-bindings-in-swiftui

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+/*
+ Take aways:
+ 
+ didSet -- Triggers only on regular vars...not @State
+ To use didSet with an @State property wrapper, create it yourself using Bidning.
+ */
 struct ContentView: View {
+    
+    @State private var blurAmount: CGFloat = 0 {
+        didSet {
+            print("New Value is \(blurAmount)")
+        }
+    }
+    
     var body: some View {
+        
+        let blur = Binding<CGFloat> (
+            get: {
+                self.blurAmount
+            },
+        set: {
+            self.blurAmount = $0
+            print("New value is \(self.blurAmount)")
+        }
+    )
+        
         Text("Hello, world!")
             .padding()
+            .blur(radius: blurAmount)
+        Slider(value: blur, in: 0...20)
     }
 }
 
