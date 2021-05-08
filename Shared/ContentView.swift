3fca075cb0f122b99a232a9191828c7dc171950a
Erik Mikac <mikacew@gmail.com>
2021-05-08 07:29:52 -0400

    completed tutorial. https://www.hackingwithswift.com/books/ios-swiftui/integrating-core-image-with-swiftui

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,40 @@
 //
 
 import SwiftUI
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
 struct ContentView: View {
+    
+    @State private var image: Image?
     var body: some View {
+        
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+        
         Text("Hello, world!")
             .padding()
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "nature.jpg") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.crystallize()
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        currentFilter.radius = 200
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
