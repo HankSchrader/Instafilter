c8c28e6fc9d1fb4e4594c86de07eb341c8913289
Erik Mikac <mikacew@gmail.com>
2021-05-12 08:26:34 -0400

    Completed coordinates tutorial.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+class ImageSave: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save Finished!")
+    }
+}
+
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+
+            Button("Select Image") {
+                self.showingImagePicker = true
+            }
+        }
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
+        }
+    }
+    
+    
+    func loadImage() {
+        guard let inputImage = inputImage else {return}
+        image = Image(uiImage: inputImage)
+        UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
     }
 }
 
